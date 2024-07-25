479e64e30e937fc87406cac2ea27327f08b61ecf
Krystian Schmidt <krystian.schmidt@siemens.com>
2024-07-25 14:02:19 +0200

    Initial commit

---
+++ src/main/java/com/example/spuelmaschine/SpuelmaschineApplication.java
@@ -0,0 +1,13 @@
+package com.example.spuelmaschine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpuelmaschineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpuelmaschineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spuelmaschine/SpuelmaschineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spuelmaschine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpuelmaschineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
